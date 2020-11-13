6217fd04c020d0e182c85aecc0a1cc3129c7bebf
Shy L <shylee873@gmail.com>
2020-11-12 22:31:38 -0500

    Added files and set up javascript alert

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click to Enter</p>
+
+<button onclick="myFunction()">ok</button>
+
+<script>
+function myFunction() {
+  alert("Welcome to my coding journey!");
+}
+</script>
+
+</body>
+</html>
