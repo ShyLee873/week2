c27acf38f0a73228d232dcc3bfcdc2306c97c75d
Shy L <shylee873@gmail.com>
2020-11-17 21:13:02 -0500

    adding corrected files and corrected javascript alert

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<p>Click to Enter</p>
-
-<button onclick="myFunction()">ok</button>
-
 <script>
-function myFunction() {
-  alert("Welcome to my coding journey!");
-}
+  function myFunction() {
+    alert("Welcome to my coding journey!")}
+  
 </script>
-
-</body>
-</html>
